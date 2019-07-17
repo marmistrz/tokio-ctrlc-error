7b6790ec1a3b5edf92f7091b0635fc21b4d27e78
Marcin Mielniczuk <marmistrz.dev@zoho.eu>
2019-07-17 16:24:32 +0200

    Improve

--- src/lib.rs
+++ src/lib.rs
@@ -21,48 +21,31 @@
 //! ```
 
 use failure::Fail;
-use futures::prelude::*;
-use std::marker::PhantomData;
+use futures::{prelude::*, FlattenStream};
 use tokio_signal::{IoFuture, IoStream};
 
 #[derive(Debug, Fail)]
 #[fail(display = "keyboard interrupt")]
 pub struct KeyboardInterrupt;
 
-pub struct CtrlcAsError<F, E>
-where
-    F: Future,
-    E: From<KeyboardInterrupt> + From<F::Error>,
-{
-    ctrlc: IoStream<()>,
+#[derive(Debug, Fail)]
+#[fail(display = "I/O error handling ctrl+c: {}", _0)]
+pub struct IoError(std::io::Error);
+
+pub struct CtrlcAsError<F> {
+    ctrlc: FlattenStream<IoFuture<IoStream<()>>>,
     future: F,
-    phantom: PhantomData<E>,
 }
 
-impl<F, E> CtrlcAsError<F, E>
+impl<F: Future> Future for CtrlcAsError<F>
 where
-    F: Future,
-    E: From<KeyboardInterrupt> + From<F::Error>,
+    F::Error: From<KeyboardInterrupt> + From<IoError>,
 {
-    fn from_future(future: F) -> Self {
-        Self {
-            ctrlc: Box::new(tokio_signal::ctrl_c().flatten_stream()),
-            future,
-            phantom: PhantomData,
-        }
-    }
-}
-
-impl<F, E> Future for CtrlcAsError<F, E>
-where
-    F: Future,
-    E: From<KeyboardInterrupt> + From<F::Error>,
-{
-    type Error = E;
+    type Error = F::Error;
     type Item = F::Item;
 
     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
-        let ctrlc_fut = self.ctrlc.poll().expect("ctrlc handling failed");
+        let ctrlc_fut = self.ctrlc.poll().map_err(IoError)?;
         if ctrlc_fut.is_ready() {
             Err(KeyboardInterrupt.into())
         } else {
@@ -71,21 +54,54 @@ where
     }
 }
 
-pub trait FutureExt<F, E>
-where
-    F: Future,
-    E: From<KeyboardInterrupt> + From<F::Error>,
-{
+pub trait FutureExt<F: Future> {
     /// Intercept ctrl+c during execution and return an error in such case.
-    fn ctrlc_as_error(self) -> CtrlcAsError<F, E>;
+    fn ctrlc_as_error(self) -> CtrlcAsError<F>;
 }
 
-impl<F, E> FutureExt<F, E> for F
+impl<F: Future> FutureExt<F> for F
 where
-    F: Future<Error = failure::Error> + 'static,
-    E: From<KeyboardInterrupt> + From<F::Error>,
+    F::Error: From<KeyboardInterrupt> + From<IoError>,
 {
-    fn ctrlc_as_error(self) -> CtrlcAsError<F, E> {
-        CtrlcAsError::from_future(self)
+    fn ctrlc_as_error(self) -> CtrlcAsError<F> {
+        CtrlcAsError {
+            ctrlc: tokio_signal::ctrl_c().flatten_stream(),
+            future: self,
+        }
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::FutureExt;
+    use futures::prelude::*;
+
+    // Test if it compiles when used with the multi-threaded runtime
+    #[test]
+    fn test_send_future() {
+        use tokio::runtime::Runtime;
+        fn get_fut() -> Box<dyn Future<Item = (), Error = failure::Error> + Send> {
+            let f = futures::future::ok(());
+            Box::new(f)
+        }
+
+        let future = get_fut().ctrlc_as_error();
+        let mut rt = Runtime::new().unwrap();
+        rt.block_on(future).unwrap();
+    }
+
+    // Test if it compiles when used with the single-threaded runtime
+    #[test]
+    fn test_non_send_future() {
+        use tokio::runtime::current_thread::Runtime;
+        fn get_fut() -> Box<dyn Future<Item = (), Error = failure::Error>> {
+            let f = futures::future::ok(());
+            Box::new(f)
+        }
+
+        let future = get_fut().ctrlc_as_error();
+        let mut rt = Runtime::new().unwrap();
+        rt.block_on(future).unwrap();
+    }
+
 }
